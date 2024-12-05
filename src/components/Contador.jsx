e29b3292c3222f2bdb56bcf8904d6c0226cfe388
Emilse Arias <emilsearias@gmail.com>
2024-12-05 11:31:51 -0300

    agregue un ejemplo de como desestructurar

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,9 +1,9 @@
 import { Button } from "react-bootstrap";
 
-const Contador = (props) => {
+const Contador = ({infoComision}) => {
     return (
         <section className="text-center">
-            <h2 >Contador: {props.infoComision} </h2>
+            <h2 >Contador: {infoComision} </h2>
             <p className="lead">1</p>
             <Button className="me-2">+</Button>
             <Button variant="danger">-</Button>
